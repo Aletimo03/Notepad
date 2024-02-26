41c10ab7bd39a47c8cc035e8301bc1b5d26058a7
Aletimo03 <alessio.timofte@sutd.unifi.it>
2024-02-26 09:02:25 +0100

    main updated

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
-
+#include "Nota.h"
+#include "Collection.h"
+#include "QuantityObserver.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Nota A("nota 1", "txt");
+    Nota B("nota 2", "txt");
+    Nota C("nota 3", "txt");
+    Nota D("nota 4", "txt");
+
+
+    std::cout << std::endl;
+    std::cout << A.getTitle() << std::endl ;
+
+    std::cout << std::endl;
+    std:: cout << ".........................." <<  std::endl ;
+    std::cout << std::endl;
+
+
+    Collection collezione("Collezione");
+
+    collezione.addNote(A);
+
+    collezione.addNote(B);
+
+    collezione.addNote(C);
+
+
+    QuantityObserver Q(&collezione);
+
+    collezione.deleteNote(B);
+
+    std::cout << std::endl;
+
+    std:: cout << ".........................." <<  std::endl ;
+
+    std::cout << std::endl;
+
+    collezione.addNote(D);
+
 }
